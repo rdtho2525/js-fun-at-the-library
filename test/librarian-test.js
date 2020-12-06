1312d9d96d9202db9821c49122cedcfbfd83c4cb
Reggie Thompson <reggie.thompson4@gmail.com>
2020-12-06 16:22:29 -0700

    Remove .skip method from lines 9, 15, 21, 27, and 35

--- test/librarian-test.js
+++ test/librarian-test.js
@@ -6,25 +6,25 @@ var {
 } = require('../src/library.js')
 
 describe('Librarian (class)', function() {
-  it.skip('should instantiate a librarian object', function() {
+  it('should instantiate a librarian object', function() {
     var casey = new Librarian("Casey");
 
     assert.instanceOf(casey, Librarian);
   });
 
-  it.skip('should have a name', function() {
+  it('should have a name', function() {
     var librarian = new Librarian("Casey");
 
     assert.equal(librarian.name, "Casey");
   });
 
-  it.skip('should be able to have a different name', function() {
+  it('should be able to have a different name', function() {
     var librarian = new Librarian("Scott");
 
     assert.equal(librarian.name, "Scott");
   });
 
-  it.skip('should work at a library', function() {
+  it('should work at a library', function() {
     var library = createLibrary("Denver Public Library");
     var librarian = new Librarian("Scott", library);
 
@@ -32,7 +32,7 @@ describe('Librarian (class)', function() {
     assert.deepEqual(librarian.library, library);
   });
 
-  it.skip('should be able to work at a different library', function() {
+  it('should be able to work at a different library', function() {
     var library = createLibrary("Aurora Public Library");
     var librarian = new Librarian("Casey", library);
 
