c36f2b94a2144674fcf64bc9456850386042e527
Reggie Thompson <reggie.thompson4@gmail.com>
2020-12-06 20:56:07 -0700

    Remove .skip methods for testing

--- test/librarian-test.js
+++ test/librarian-test.js
@@ -39,14 +39,14 @@ describe('Librarian (class)', function() {
     assert.deepEqual(librarian.library, library);
   });
 
-  it.skip('should be able to greet a library patron by name', function() {
+  it('should be able to greet a library patron by name', function() {
     var scott = new Librarian("Scott");
 
     assert.equal(scott.greetPatron("Will"), "Hello, Will!");
     assert.equal(scott.greetPatron("Tilly"), "Hello, Tilly!");
   });
 
-  it.skip('should greet a library patron differently if it is morning', function() {
+  it('should greet a library patron differently if it is morning', function() {
     var leta = new Librarian("Leta");
 
     assert.equal(leta.greetPatron("Scott"), "Hello, Scott!");
@@ -54,7 +54,7 @@ describe('Librarian (class)', function() {
     assert.equal(leta.greetPatron("Casey", false), "Hello, Casey!");
   });
 
-  it.skip('should confirm if a requested book is on the shelves', function() {
+  it('should confirm if a requested book is on the shelves', function() {
     var auroraLibrary = createLibrary("Aurora Public Library");
     var librarian = new Librarian("Casey", auroraLibrary);
     var book = {title: 'The Fifth Season', genre: 'fantasy'}
